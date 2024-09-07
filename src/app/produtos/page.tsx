0e56b3325df08765abd384b1210ac8ac94499295
Matheus Sleutjes <matheus.sleutjes@loglabdigital.com.br>
2024-09-07 19:42:44 -0400

    pagina produtos

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Produtos() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>page initial</h1>
+        <h1>produtos</h1>
       </main>
     </div>
   );
