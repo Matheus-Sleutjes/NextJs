08a0138579816b2423c720cdb16f7819e67a76e9
BrunoFaria-DEV <developer.brunofaria@gmail.com>
2024-09-08 19:50:06 -0400

    new project template

---
+++ src/app/dashboard/produtos/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Produtos() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>produtos</h1>
+      </main>
+    </div>
+  );
+}
--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+import styles from "../_css/site.module.css";
 
 export default function Home() {
   return (
