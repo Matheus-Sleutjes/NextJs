8ac6f56d0305cdafd74db6e0814d904f9218d06b
Bruno Faria <developer.brunofaria@gmail.com>
2024-09-15 22:52:01 -0400

    sixth commit

--- src/app/dashboard/produtos/page.tsx
+++ src/app/dashboard/produtos/page.tsx
@@ -1,5 +1,12 @@
-
+import DashboardLayout from "../../_components/layouts/dashboard/layout";
 
 export default function Produtos() {
-  
+    return (
+        <DashboardLayout>
+          <div>
+            <h1>Produtos</h1>
+            <p>Esta é a página de produtos.</p>
+          </div>
+        </DashboardLayout>
+    );
 }
