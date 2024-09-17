3f059a4d442bb35d127545377a1f0186ef2df656
Bruno Faria <developer.brunofaria@gmail.com>
2024-09-16 20:17:10 -0400

    sevent commit

--- src/app/_components/shared/sidebar/page.tsx
+++ src/app/_components/shared/sidebar/page.tsx
@@ -1,66 +1,70 @@
+import Image from "next/image"; 
+import ifmt from '../../../_img/ifmt.webp';
+
 export default function Sidebar() {
-    return  <aside className="sidebar">
-    <div className="sidebar-header">
-      <img src="images/logo.png" alt="logo" />
-      <h2>CodingLab</h2>
-    </div>
-    <ul className="sidebar-links">
-      <h4>
-        <span>Main Menu</span>
-        <div className="menu-separator"></div>
-      </h4>
-      <li>
-        <a href="#">
-          <span className="material-symbols-outlined"> dashboard </span>Dashboard</a>
-      </li>
-      <li>
-        <a href="#"><span className="material-symbols-outlined"> overview </span>Overview</a>
-      </li>
-      <li>
-        <a href="#"><span className="material-symbols-outlined"> monitoring </span>Analytic</a>
-      </li>
-      <h4>
-        <span>General</span>
-        <div className="menu-separator"></div>
-      </h4>
-      <li>
-        <a href="#"><span className="material-symbols-outlined"> folder </span>Projects</a>
-      </li>
-      <li>
-        <a href="#"><span className="material-symbols-outlined"> groups </span>Groups</a>
-      </li>
-      <li>
-        <a href="#"><span className="material-symbols-outlined"> move_up </span>Transfer</a>
-      </li>
-      <li>
-        <a href="#"><span className="material-symbols-outlined"> flag </span>All Reports</a>
-      </li>
-      <li>
-        <a href="#"><span className="material-symbols-outlined">
-            notifications_active </span>Notifications</a>
-      </li>
-      <h4>
-        <span>Account</span>
-        <div className="menu-separator"></div>
-      </h4>
-      <li>
-        <a href="#"><span className="material-symbols-outlined"> account_circle </span>Profile</a>
-      </li>
-      <li>
-        <a href="#"><span className="material-symbols-outlined"> settings </span>Settings</a>
-      </li>
-      <li>
-        <a href="#"><span className="material-symbols-outlined"> logout </span>Logout</a>
-      </li>
-    </ul>
-    <div className="user-account">
-      <div className="user-profile">
-        <img src="images/profile-img.jpg" alt="Profile Image" />
-        <div className="user-detail">
-          <h3>Eva Murphy</h3>
-          <span>Web Developer</span>
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+      <Image src={ifmt} height={50}/>
+        <h2>EsaLab</h2>
+      </div>
+      <ul className="sidebar-links">
+        <h4>
+          <span>Menu Principal</span>
+          <div className="menu-separator"></div>
+        </h4>
+        <li>
+          <a href="#">
+            <span className="material-symbols-outlined"> dashboard </span>Painel</a>
+        </li>
+        <li>
+          <a href="#"><span className="material-symbols-outlined"> overview </span>Visão Geral</a>
+        </li>
+        <li>
+          <a href="#"><span className="material-symbols-outlined"> monitoring </span>Análises</a>
+        </li>
+        <h4>
+          <span>Geral</span>
+          <div className="menu-separator"></div>
+        </h4>
+        <li>
+          <a href="#"><span className="material-symbols-outlined"> folder </span>Projetos</a>
+        </li>
+        <li>
+          <a href="#"><span className="material-symbols-outlined"> groups </span>Grupos</a>
+        </li>
+        <li>
+          <a href="#"><span className="material-symbols-outlined"> move_up </span>Transferências</a>
+        </li>
+        <li>
+          <a href="#"><span className="material-symbols-outlined"> flag </span>Relatórios</a>
+        </li>
+        <li>
+          <a href="#"><span className="material-symbols-outlined">
+              notifications_active </span>Notificações</a>
+        </li>
+        <h4>
+          <span>Conta</span>
+          <div className="menu-separator"></div>
+        </h4>
+        <li>
+          <a href="#"><span className="material-symbols-outlined"> account_circle </span>Perfil</a>
+        </li>
+        <li>
+          <a href="#"><span className="material-symbols-outlined"> settings </span>Configurações</a>
+        </li>
+        <li>
+          <a href="#"><span className="material-symbols-outlined"> logout </span>Sair</a>
+        </li>
+      </ul>
+      <div className="user-account">
+        <div className="user-profile">
+          <div className="user-detail">
+            <h3>Grupo-2</h3>
+            <span>Next-Js</span>
+          </div>
         </div>
       </div>
-    </div>
-  </aside>
-  }
+    </aside>
+  );
+}
